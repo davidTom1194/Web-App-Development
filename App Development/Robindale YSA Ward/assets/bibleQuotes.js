ebd92903c39a02a63324dcb4bd300474b2edd5a1
David Tom <davidtom1194@gmail.com>
2024-05-10 18:36:14 -0700

    Add files via upload

---
+++ App Development/Robindale YSA Ward/assets/bibleQuotes.js
@@ -0,0 +1,10 @@
+// assets/bibleQuotes.js
+
+  // Array of random Bible quotes
+export const bibleQuotes = [
+  "The Lord is my shepherd; I shall not want. - Psalm 23:1",
+  "I can do all things through Christ who strengthens me. - Philippians 4:13",
+  "For I know the plans I have for you, declares the Lord, plans to prosper you and not to harm you, plans to give you hope and a future. - Jeremiah 29:11",
+  "Trust in the Lord with all your heart and lean not on your own understanding. - Proverbs 3:5",
+  "Be strong and courageous. Do not be afraid; do not be discouraged, for the Lord your God will be with you wherever you go. - Joshua 1:9"
+];
